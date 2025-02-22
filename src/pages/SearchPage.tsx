feac7b8af7ef2fe3c05c22227f324dda5965a5d6
Jeff Ma <jucjeff96@gmail.com>
2025-02-22 15:56:17 -0600

    feat: add place holder tabs

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,10 +1,26 @@
-import { LogoutButton } from "@/components/LogoutButton";
+import { LogoutButton } from "@/components/Authentication/LogoutButton";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function SearchPage() {
   return (
     <div>
-      <h1>Search Page</h1>
-      <LogoutButton />
+      <div className="flex flex-col sticky top-0 py-8 w-full">
+        <h1>Search Page</h1>
+        <Tabs
+          defaultValue="all-dogs"
+          className="py-4 w-full flex align-center md:w-[400px]"
+        >
+          <TabsList>
+            <TabsTrigger value="all-dogs">All Dogs 🐕</TabsTrigger>
+            <TabsTrigger value="favorite-dogs">Favorite Dogs ❤️</TabsTrigger>
+          </TabsList>
+          <TabsContent value="all-dogs">Dogs</TabsContent>
+          <TabsContent value="favorite-dogs">Favorite Dogs</TabsContent>
+        </Tabs>
+        <div>
+          <LogoutButton />
+        </div>
+      </div>
     </div>
   );
 }
